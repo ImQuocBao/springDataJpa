5f75d88e1eabd1e655b221266e8513ecc2770773
BaoDoan(Yone) <baodakmil123@gmail.com>
2022-09-19 12:57:45 +0700

    first commit

--- src/main/java/com/example/spingdatajpa/SpingDataJpaApplication.java
+++ src/main/java/com/example/spingdatajpa/SpingDataJpaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpingDataJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpingDataJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpingDataJpaApplication.class, args);
+    }
 
 }
