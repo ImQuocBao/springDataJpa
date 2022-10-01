92d041a322cf720ccecee0c5ea22dc2b6f6392e4
BaoDoan(Yone) <baodakmil123@gmail.com>
2022-10-01 10:13:55 +0700

    finish config security with jwt

---
+++ src/main/java/com/example/spingdatajpa/controller/AuthenticationController.java
@@ -0,0 +1,8 @@
+package com.example.spingdatajpa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+}
